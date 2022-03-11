c033d810608b14944ba5abc9658c11799c8278e8
yoonseok <ham5312@bearrobotics.ai>
2022-03-11 11:38:24 -0800

    2022-03-11

---
+++ javascript/10_function.js
@@ -0,0 +1,26 @@
+// function
+function nameMaker (name) {
+	let newName = name || 'noName';
+	return `Hi my name is ${newName}` + '!' + 10;
+}
+
+function nameMakerWithDefaultValue (name = 'noName') {
+	let newName = name;
+	return `Hi my name is ${newName}` + '!' + 10;
+}
+
+const myName = nameMaker('yoonseok');
+const herName = nameMaker('Jane');
+const noName = nameMaker();
+const noNameDefault = nameMakerWithDefaultValue();
+
+console.log(myName);
+console.log(herName);
+console.log(noName);
+console.log(noNameDefault);
+
+function noReturn () {
+	return
+}
+
+console.log(noReturn())
