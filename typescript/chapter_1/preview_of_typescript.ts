dbe18b5c60490ec7d23c204e24f49ed965880d64
yoonseok <ham5312@bearrobotics.ai>
2022-05-22 17:44:22 -0700

    promise

--- typescript/chapter_1/preview_of_typescript.ts
+++ typescript/chapter_1/preview_of_typescript.ts
@@ -16,7 +16,41 @@ let b = 2;
 // fyi :https://www.clien.net/service/board/cm_app/14670849
 [a, b] = [b, a]
 console.log(a, b)
+// arrow function
 const add = (a: number, b: number): number => {
     return a + b
 }
 console.log(add(a, b))
+
+// class
+abstract class Animal {
+    constructor(public name?: string, public age?: number) { }
+    abstract say(): string
+}
+class Cat extends Animal {
+    say() { return `${age} meow` }
+}
+class Dog extends Animal {
+    say() { return `${this.name} bark` }
+}
+let animals: Animal[] = [new Cat("street", 11), new Dog('sigoal', 10)]
+console.log(animals.map(a => a.say()))
+
+// yield (generator)
+function* gen() {
+    yield ['hi', 1, 2, 3, 4]
+}
+
+for (let value of gen()) {
+    console.log(value)
+}
+
+//Promise and async/await
+let get = async () => {
+    let values = []
+    for (let i = 0; i < 4; i++) {
+        values.push(await Promise.resolve(i))
+    }
+    return values
+}
+get().then(values => console.log(values))
