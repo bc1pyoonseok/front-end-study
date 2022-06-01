49b85e4721101beaedfb4081a4f7bd367d9d270c
yoonseok <ham5312@bearrobotics.ai>
2022-06-01 00:42:41 -0700

    6-1

---
+++ Typescript/chapter_6/src/RangeIterable.ts
@@ -0,0 +1,31 @@
+import { nextTick } from "process"
+import { NamedExportBindings } from "typescript"
+
+export class RangeIterable implements Iterable<number>{
+    constructor(public start: number, public end: number) { }
+    [Symbol.iterator](): Iterator<number> {
+        const that = this
+        let currentNum: number = this.start
+        const End: number = this.end
+        const iterator = {
+            next() {
+                let value = currentNum < End ? currentNum++ : undefined
+                let done: boolean = value == undefined
+                return { value, done }
+            }
+        }
+        console.log(iterator)
+        return iterator
+    }
+}
+
+let a = new RangeIterable(10, 22)
+let iter = a[Symbol.iterator]()
+while (true) {
+    const { value, done } = iter.next()
+    if (done) break
+    console.log(value)
+}
+for (let i of new RangeIterable(0, 9)) {
+    console.log(i)
+}
