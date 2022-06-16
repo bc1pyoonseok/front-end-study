b8d54e0eae68d153a1f2a213be4570928199bfd5
yoonseok <ham5312@bearrobotics.ai>
2022-06-16 00:34:44 -0700

    ramda

--- Typescript/chapter_9/src/9-2_ramda.ts
+++ Typescript/chapter_9/src/9-2_ramda.ts
@@ -1,4 +1,4 @@
-import { range, tap, pipe, add, curryN } from 'ramda'
+import { range, tap, pipe, add, curryN, or } from 'ramda'
 
 console.log(range(10, 101))
 tap((value) => console.log(value))(range(10, 101))
@@ -25,6 +25,16 @@ pipe(
 console.log(array)
 let a = add(1)
 console.log(a(10), add(10, 11))
-let c = curryN(3, (...numbers:number[]):number => numbers.reduce((sum, current) => sum + current, 0))
-console.log(c(4,2,3))
+let c = curryN(3, (...numbers: number[]): number => numbers.reduce((sum, current) => sum + current, 0))
+console.log(c(4, 2, 3))
 console.log(c(4)(2)(11))
+
+let originalArray = new Array(1, 2, 3, 4, 5)
+const adder = (value: number): number => value + 1
+originalArray = originalArray.map(adder)
+
+const resultArray = pipe(
+    (array) => { return array.map(add(100)) },
+    (array) => { return array.filter((value: number) => value % 2) }
+)(originalArray)
+console.log(resultArray)
