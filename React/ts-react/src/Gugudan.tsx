e09bda2e3657fa2753e2c07b8c829d110249eeec
yoonseok <ham5312@bearrobotics.ai>
2022-08-21 15:51:22 +0900

    react study

--- React/ts-react/src/Gugudan.tsx
+++ React/ts-react/src/Gugudan.tsx
@@ -1,44 +1,52 @@
-import React from 'react';
-import { createInputFiles } from 'typescript';
+import React from "react";
 
 const GuguDan: React.FC = () => {
-    const getRandomNumber = (): number => {
-        return Math.floor(Math.random() * 11);
-    }
-    const [first, setFirst] = React.useState<number>(getRandomNumber())
-    const [second, setSecond] = React.useState<number>(getRandomNumber())
-    const [value, setValue] = React.useState<string>('')
-    const [result,setResult] =  React.useState<boolean | string>('init')
-    const [correct,setCorrect] =  React.useState<number>(0)
+  const getRandomNumber = (): number => {
+    return Math.floor(Math.random() * 11);
+  };
+  const [first, setFirst] = React.useState<number>(getRandomNumber());
+  const [second, setSecond] = React.useState<number>(getRandomNumber());
+  const [value, setValue] = React.useState<string>("");
+  const [answer, setAnswer] = React.useState<number>(first * second);
+  const [result, setResult] = React.useState<boolean | string>("init");
+  const [correct, setCorrect] = React.useState<number>(0);
 
-    return (
-        <>
-            <div>what is {first} x {second}?</div>
-            <form onSubmit={(e)=>{
-                e.preventDefault();
-                if(parseInt(`${value}`)===first*second){
-                    setResult(true)
-                    setFirst(getRandomNumber())
-                    setSecond(getRandomNumber())
-                    setValue('')
-                    setCorrect(correct+1)
-                }
-                else{
-                    setResult(false)
-                }
-            }}>
-                <input type="number" value={value} onChange={(e) => {setValue(e.target.value)}} />
-                <button >input</button>
-            </form>
-            <div>
-                {result==='init' && <>take a look</>}
-                {result===true && <>correct!</>}
-                {result===false && <>worng!</>}
-                <br/>
-                <>I got {correct} times!</>
-            </div>
-        </>
-    );
-}
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (parseInt(`${value}`) === first * second) {
+      setResult(true);
+      setFirst(getRandomNumber());
+      setSecond(getRandomNumber());
+      setAnswer(first * second);
+      setValue("");
+      setCorrect(correct + 1);
+    } else {
+      setResult(false);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        what is {first} x {second}?
+      </div>
+      <form onSubmit={onSubmit}>
+        <input type="number" value={value} onChange={onChange} />
+        <button>input</button>
+      </form>
+      <div>
+        {result === "init" && <>take a look</>}
+        {result === true && <>{answer} correct!</>}
+        {result === false && <>worng!</>}
+        <br />
+        <>I got {correct} times!</>
+      </div>
+    </>
+  );
+};
 
 export default GuguDan;
--- React/ts-react/src/index.tsx
+++ React/ts-react/src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GuguDan from './Gugudan';
-import Menu from './Menu';
+import React from "react";
+import ReactDOM from "react-dom";
+import GuguDan from "./Gugudan";
+import Menu from "./Menu";
 
 ReactDOM.render(
   <React.StrictMode>
     <Menu />
-    <GuguDan/>
+    <GuguDan />
+    <GuguDan />
+    <GuguDan />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
