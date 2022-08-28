8061857890c66fee2115bd91918326729cbbe278
yoonseok <ham5312@bearrobotics.ai>
2022-08-28 16:04:31 +0900

    operator

---
+++ modern-javascript-deep-dive/pow_operator.ts
@@ -0,0 +1,7 @@
+console.log(Math.pow(2, 2));
+console.log(2 ** 2);
+console.log((-5) ** 2);
+console.log((-5) ** 0);
+console.log(5 ** 2.5);
+// pow operator has top priority
+console.log(2 * 5 ** 2); // 50
