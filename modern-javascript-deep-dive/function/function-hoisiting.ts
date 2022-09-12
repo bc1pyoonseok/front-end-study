894ab80b6e1a5e84314e2b4efdd3c96be935cef7
yoonseok <ham5312@bearrobotics.ai>
2022-09-12 16:52:48 +0900

    function

---
+++ modern-javascript-deep-dive/function/function-hoisiting.ts
@@ -0,0 +1,24 @@
+// function
+function add(a: number, b: number): number {
+  return a + b;
+}
+// function literal
+let a = function add(a: number, b: number): number {
+  return a + b;
+};
+// if you use fuction literal, you can skip function name
+let b = function (a: number, b: number): number {
+  return a + b;
+};
+// these are also literal
+(function add(a: number, b: number): number {
+  return a + b;
+})(4, 5);
+(function (a: number, b: number): number {
+  return a + b;
+})(4, 5);
+
+// function declare verses functuion literal.
+// function declare already runned before runtime.
+
+// function hoisiting is different from variable hoisiting.
