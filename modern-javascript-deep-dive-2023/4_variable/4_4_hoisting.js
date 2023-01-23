5bcfd76b3690fc914ec89681bca5d610c257d371
yoonseok <ham5312@bearrobotics.ai>
2023-01-24 01:07:00 +0900

    chapter_4

---
+++ modern-javascript-deep-dive-2023/4_variable/4_4_hoisting.js
@@ -0,0 +1,13 @@
+// undefined
+console.log(score);
+
+var score;
+
+// defining variable, function and class is executed before runtime
+
+console.log(a)
+a()
+
+function a() {
+    console.log('hi Im a')
+}
---
+++ modern-javascript-deep-dive-2023/4_variable/4_5_assignment.js
@@ -0,0 +1,19 @@
+var score;
+score = 80;
+
+// this is same as
+
+var score = 80;
+
+// var score; is executed before runtime
+// score = 80; is executed at runtime
+
+console.log(example);
+var example = "defining value is executed before runtime";
+console.log(example);
+
+console.log(example2);
+example2 = 80;
+var example2;
+
+console.log(example2);
